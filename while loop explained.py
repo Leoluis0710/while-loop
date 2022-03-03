8ffaad12f28988fa14df53c233145254a03663ad
Leoluis0710 <98952616+Leoluis0710@users.noreply.github.com>
2022-03-03 12:02:38 +0000

    Add files via upload

--- while loop explained.py
+++ while loop explained.py
@@ -1,5 +1,4 @@
-answer='N'
-while answer != 'Y':
-    print('are we there yet?')
-    answer = input('please respond with Y or N')
-print('yay we are there ! ')
+username = ''
+while not username:
+    username = input('please entyer a username')
+    
